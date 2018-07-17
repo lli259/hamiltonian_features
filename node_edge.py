5f31565a89b454e8961874e2a6ec7c880b16c6e7
lli259 <37081135+lli259@users.noreply.github.com>
2018-07-17 11:59:04 -0400

    add print information

--- node_edge.py
+++ node_edge.py
@@ -26,14 +26,19 @@ def get_ratio_node_edge(G):
 #graph=graph_parse.read_ham_graph("instance_test_3nodes.lp")
 graph=graph_parse.read_ham_graph(sys.argv[1])
 #print graph
-
+print "get_num_of_nodes(graph),get_num_of_edges(graph),get_ratio_node_edge(graph)"
 print get_num_of_nodes(graph),get_num_of_edges(graph),get_ratio_node_edge(graph)
 
 #------------------------
 reverse_graph=reverse(graph)
 #print reverse_graph
-
+print "get_num_of_nodes(reverse_graph),get_num_of_edges(reverse_graph),get_ratio_node_edge(reverse_graph)"
 print get_num_of_nodes(reverse_graph),get_num_of_edges(reverse_graph),get_ratio_node_edge(reverse_graph)
+
+
+
+
+
 
 def get_bi_edge(G):
 	bi_graph={}
@@ -42,5 +47,5 @@ def get_bi_edge(G):
 		for edge in G[node]:
 			if node in G[edge]:
 				bi_graph[node].append(edge)
-	return  get_num_of_edges(bi_graph)/2
+	return  get_num_of_edges(bi_graph)
 print "bi_edge:",get_bi_edge(graph)
