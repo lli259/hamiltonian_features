301ba5ace6025d81493d6ba83ee0378138020f16
lli259 <37081135+lli259@users.noreply.github.com>
2018-07-17 16:18:30 -0400

    Add files via upload

--- node_edge.py
+++ node_edge.py
@@ -23,6 +23,16 @@ def get_ratio_node_edge(G):
 	num_of_edges=get_num_of_edges(G)
 	return float(num_of_nodes)/num_of_edges
 
+
+def get_min_out_degree(G):
+	return min([len(G[n]) for n in G.keys()])
+
+def get_max_out_degree(G):
+	return max([len(G[n]) for n in G.keys()])
+
+def get_avg_out_degree(G):
+	return sum([len(G[n]) for n in G.keys()])/float(len(G.keys()))
+
 #graph=graph_parse.read_ham_graph("instance_test_3nodes.lp")
 graph=graph_parse.read_ham_graph(sys.argv[1])
 #print graph
@@ -47,5 +57,10 @@ def get_bi_edge(G):
 		for edge in G[node]:
 			if node in G[edge]:
 				bi_graph[node].append(edge)
-	return  get_num_of_edges(bi_graph)
-print "bi_edge:",get_bi_edge(graph)
+	return  bi_graph
+
+print "bi_edge:",get_num_of_edges(get_bi_edge(graph))
+
+print "min_out_degree:",get_min_out_degree(graph),"max_out_degree:",get_max_out_degree(graph),"avg_out_degree:",get_avg_out_degree(graph)
+print "min_in_degree:",get_min_out_degree(reverse_graph),"max_in_degree:",get_max_out_degree(reverse_graph),"avg_out_degree:",get_avg_out_degree(reverse_graph)
+
